fc6806eec0c49cbfb1f2962d17f017537684ead8
weixinxu666 <40337178+weixinxu666@users.noreply.github.com>
2019-04-23 10:23:46 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+import argparse
+from glob import glob
+import tensorflow as tf
+import time
+from model import denoiser
+import os
+import numpy as np
+
+import os
+# os.environ["TF_CPP_MIN_LOG_LEVEL"]='1' # 这是默认的显示等级，显示所有信息
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='2' # 只显示 warning 和 Error
+# os.environ["TF_CPP_MIN_LOG_LEVEL"]='3' # 只显示 Error
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--epoch', dest='epoch', type=int, default=10, help='# of epochs')
+parser.add_argument('--batch_size', dest='batch_size', type=int, default=128, help='# images in batch')
+parser.add_argument('--lr', dest='lr', type=float, default=0.001, help='initial learning rate for adam')
+# parser.add_argument('--use_gpu', dest=False, type=int, default=1, help='gpu flag, 1 for GPU and 0 for CPU')
+# parser.add_argument('--phase', dest='phase', default='test', help='train or test')
+parser.add_argument('--checkpoint_dir', dest='ckpt_dir', default='./checkpoint_demo', help='models are saved here')
+parser.add_argument('--test_dir', dest='test_dir', default='./data/denoised', help='denoised sample are saved here')
+args = parser.parse_args()
+
+
+def denoiser_train(denoiser, lr):
+        noisy_eval_files = glob('./data/train/noisy/*.png')
+        noisy_eval_files = sorted(noisy_eval_files)
+        eval_files = glob('./data/train/original/*.png')
+        eval_files = sorted(eval_files)
+        denoiser.train(eval_files, noisy_eval_files, batch_size=args.batch_size, ckpt_dir=args.ckpt_dir, epoch=args.epoch, lr=lr)
+
+
+def denoiser_test(denoiser):
+
+    noisy_eval_files = glob('./data/test/noisy/*.png')
+#    n = [int(i) for i in map(lambda x: x.split('/')[-1].split('.png')[0], noisy_eval_files)]
+#    noisy_eval_files = [x for (y, x) in sorted(zip(n, noisy_eval_files))]
+    noisy_eval_files = sorted(noisy_eval_files)
+    eval_files = glob('./data/test/original/*.png')
+    eval_files = sorted(eval_files)
+    start = time.time()
+    denoiser.test(eval_files, noisy_eval_files, ckpt_dir=args.ckpt_dir, save_dir='./data/denoised')
+    end = time.time()
+    print("Elapsed time:", end-start)
+
+def main(_):
+    if not os.path.exists(args.ckpt_dir):
+        os.makedirs(args.ckpt_dir)
+    if not os.path.exists(args.test_dir):
+        os.makedirs(args.test_dir)
+
+    lr = args.lr * np.ones([args.epoch])
+    lr[30:] = lr[0] / 10.0
+    # if args.use_gpu:
+    #     # added to control the gpu memory
+    #     print("GPU\n")
+    #     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+    #     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    #         model = denoiser(sess)
+    #         if args.phase == 'train':
+    #             denoiser_train(model, lr=lr)
+    #         elif args.phase == 'test':
+    #             denoiser_test(model)
+    #         else:
+    #             print('[!]Unknown phase')
+    #             exit(0)
+    # else:
+    # print("CPU\n")
+    with tf.Session() as sess:
+        model = denoiser(sess)
+        # if args.phase == 'train':
+        #     denoiser_train(model, lr=lr)
+        # elif args.phase == 'test':
+        denoiser_test(model)
+        # else:
+        #     print('[!]Unknown phase')
+        #     exit(0)
+
+def test():
+    # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+    with tf.Session() as sess:
+      model = denoiser(sess)
+      denoiser_test(model)
+
+  
+if __name__ == '__main__':
+    tf.app.run()
